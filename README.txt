7c0e79c56d2a85751fc0ef414a295175d1fe7b78
fly <fly@localhost.localdomain>
2012-12-30 13:03:43 +0800

    dfdf

--- README.txt
+++
@@ -1,4 +0,0 @@
-1、通过命令npm install -d来安装包依赖。
-2、通过chmod +x run.sh来赋执行权限
-3、通过命令./run.sh来启动数据抓取服务器。
-4、通过logs/out.log来查看日志信息。
--- etc/loadConfigure.js
+++
@@ -1,4 +0,0 @@
-var fs = require('fs');
-var config = __dirname + '/settings.json';
-var settings = JSON.parse(fs.readFileSync(config, 'utf8'));
-exports.configure = settings;
